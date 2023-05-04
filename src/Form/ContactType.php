c2e2f9015a32a9bd229de865fb66a07945aedf1c
diti <carmasol.dietric@laposte.net>
2023-05-04 16:03:00 +0200

    français

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,10 +3,11 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactType extends AbstractType
 {
@@ -14,9 +15,13 @@ class ContactType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('subject')
-            ->add('content', TextareaType::class)
-            ->add('confirm', SubmitType::class)
+            ->add('subject',  TextType::class, [
+                'label' =>'Sujet'])
+
+            ->add('content', TextType::class, [
+                'label' =>'Contenue'])
+
+            ->add('confirmer', SubmitType::class)
         ;
     }
 
